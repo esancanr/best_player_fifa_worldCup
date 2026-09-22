7c150a96bd3258c2db4c88c32ffb9e7871716c97
Edison <esancanr@outlook.com>
2026-09-22 14:11:22 -0400

    DATA EXTRACTION

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#--------------------------------------------------------
+# DATA EXTRACTION
+#--------------------------------------------------------
+df = pd.read_csv('data/players_22.csv', low_memory=False)
+df = df[['short_name', 'age', 'nationality_name','overall', 'potential',
+         'club_name', 'value_eur', 'wage_eur', 'player_positions']]
+
+df['player_positions'] = df['player_positions'].str.split(',', expand=True)[0]
+df.dropna(inplace=True)
+
+players_missing_games = ['K. Benzema', 'N. Kanté', 'P. Pogba', 'C. Nkunku', 'M. Maignan', 'A. Martial', 'S. Mané', 
+                         'M. Reus', 'D. Jota', 'P. Neto', 'B. Chilwell', 'R. James', 'K. Walker', 'G. Wijnaldum', 'G. Jesus',
+                         'A. Telles', 'R. Araújo', 'J. Corona', 'M. Robinson']
+
+delete_index = df[df['short_name'].isin(players_missing_games)].index
+df.drop(delete_index, axis=0, inplace=True)
+
+
+teams = ['Qatar', 'Ecuador', 'Senegal', 'Netherlands', 'England', 'Iran', 'USA', 'Wales', 'Argentina', 'Saudi Arabia', 'Mexico',
+    'Poland', 'France', 'Australia', 'Denmark', 'Tunisia','Spain', 'Costa Rica', 'Germany', 'Japan', 'Belgium', 'Canada', 'Morocco',
+    'Croatia', 'Brazil', 'Serbia', 'Switzerland','Cameroon', 'Portugal', 'Ghana', 'Uruguay','South Korea']
+df = df[df['nationality_name'].isin(teams)]
+df.sort_values(by=['overall', 'potential', 'value_eur'], ascending=False, inplace=True)
+
+#--------------------------------------------------------
+# BEST PLAYER SCORE
+#--------------------------------------------------------
